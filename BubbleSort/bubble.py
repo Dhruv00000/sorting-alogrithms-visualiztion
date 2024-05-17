1e7e96e557487489b546e49a6d1a3ae32ab6cde4
Dhruv <dhruvmitna9@gmail.com>
2024-05-17 12:47:48 +0530

    Removed audio functionality (temporarily),                              improved bar colouring, added a constraint on the amount variable (>= 3), stopped the graph from disappearing once the sorting process is completed

--- BubbleSort/bubble.py
+++ BubbleSort/bubble.py
@@ -4,10 +4,10 @@
 
 from matplotlib import pyplot, rcParams
 from numpy import random, ndarray, arange
-from winsound import Beep
+# from winsound import Beep
 
 # This variable controls the number of bars.
-amount = 20
+amount = 25
 
 rcParams["toolbar"] = "None"
 pyplot.figure(facecolor="black")
@@ -18,31 +18,41 @@ if isinstance(amount, int):
     lst: ndarray = random.randint(20, 150, amount)
     x = arange(0, amount, 1)
 
-    for i in range(len(lst)):
+    for sorted_elements, i in enumerate(range(len(lst))):
         for j in range(len(lst) - i - 1):
+
+            if amount >= 3:
+
+                barGraph = pyplot.bar(x, lst)
+                pyplot.axis('off') # Removing this line breaks the entire plot for some reason.
+
+                for barIndex in range(len(barGraph)):
+                    if (barIndex + 1) <= (len(barGraph) - sorted_elements): barGraph[barIndex].set_color("white")
+                    else: barGraph[barIndex].set_color("green")
+
+                # Beep((700 + int(800 * ((lst[j] - 20) / 130))), 40)
+
+                if lst[j] > lst[j + 1]:
+
+                    barGraph[j].set_color('b')
+                    barGraph[j + 1].set_color('r')
+
+                    lst[j], lst[j + 1] = lst[j + 1], lst[j]
+
+                else:
+                    barGraph[j].set_color('r')
+                    barGraph[j + 1].set_color('b')
+
+                pyplot.pause(0.000000001)
+                if sorted_elements < len(barGraph) - 2: pyplot.clf()
+                else:
+                    for Bar in barGraph: Bar.set_color("green")
             
-            barGraph = pyplot.bar(x, lst)
-            pyplot.axis('off') # Removing this line breaks the entire plot for some reason.
-            for Bar in barGraph: Bar.set_color("white")
+            else: print("\nAmount must be a non-zero integer greater than two.\n")
 
-            Beep((700 + int(800 * ((lst[j] - 20) / 130))), 40)
+    if amount >= 3: pyplot.show()
 
-            if lst[j] > lst[j + 1]:
-                barGraph[j].set_color('g')
-                barGraph[j + 1].set_color('r')
-                lst[j], lst[j + 1] = lst[j + 1], lst[j]
-            
-            else:
-                barGraph[j].set_color('r')
-                barGraph[j + 1].set_color('g')
-
-            pyplot.pause(0.000000001)
-            pyplot.clf()
-    
-    if amount != 0: pyplot.show()
-    else: print("\nAmount must not be zero.\n")
-
-else: print("\nAmount must be a non-zero integer\n")
+else: print("\nAmount must be a non-zero integer greater than two.\n")
 
 # n = len(lst)
 # swapped = False
